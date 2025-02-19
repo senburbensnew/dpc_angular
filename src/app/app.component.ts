8cdc6bc5f03c77ac03914f7032c8d551ae09bfba
Pierre Rubens Milorme <prubensmilorme@gmail.com>
2025-02-19 13:17:28 -0500

    configure routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports : [HomeComponent, HeaderComponent, CarouselComponent, FooterComponent],
+  imports : [HomeComponent, HeaderComponent, CarouselComponent, FooterComponent, RouterModule],
   standalone: true
 })
 export class AppComponent implements OnInit {
---
+++ src/app/menu-bar2/menu-bar2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar2',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-bar2.component.html',
+  styleUrl: './menu-bar2.component.scss'
+})
+export class MenuBar2Component {
+
+}
