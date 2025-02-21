31e7168a8e7624750d041880ca23b997d5032319
Pierre Rubens Milorme <prubensmilorme@gmail.com>
2025-02-21 11:09:38 -0500

    add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes =  [
     { path: '', component: HomeComponent },
-    { path: '**', redirectTo: '', pathMatch: 'full' } // Wildcard route for 404 handling
+    { path: 'dpc-admin', component: AdminComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
