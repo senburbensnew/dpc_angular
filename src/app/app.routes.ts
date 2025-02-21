4da505ff96ef000c612a65a19bc78fc6be17ea2f
Pierre Rubens Milorme <prubensmilorme@gmail.com>
2025-02-21 11:15:34 -0500

    add notfound pcomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 export const routes: Routes =  [
     { path: '', component: HomeComponent },
     { path: 'dpc-admin', component: AdminComponent },
     { path: 'login', component: LoginComponent },
-    { path: '**', redirectTo: '', pathMatch: 'full' }
+    { path: '**', component: NotfoundComponent },
+    // { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
